5113fa509d3b9d93299af47a004eccec1191fe98
Shwe Hlaing <shwe@pixelsquad.com.sg>
2024-08-06 17:02:45 +0800

    Improve Makefile and delete unnecessary files

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 setup:
 	@make build
 	@make up
-	@make composer
-	@make data
+	@make composer composer_cmd="update"
+	@make artisan artisan_cmd="migrate"
 up:
 	docker compose up -d
 down:
 	docker compose stop
 build:
 	docker compose build --no-cache --force-rm
+
 composer:
-	docker exec 10x bash -c "composer update"
-data:
-	docker exec 10x bash -c "php artisan migrate"
+	docker exec 10x bash -c "composer $(composer_cmd)"
+artisan:
+	docker exec 10x bash -c "php artisan $(artisan_cmd)"
+cmd:
+	docker exec $(cmd_c) bash -c "$(cmd_cmd)"
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,6 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
@@ -43,8 +42,6 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
-        dd($request);
 
         event(new Registered($user));
 
